d3959c4a5ab6b44ee64ff61a16aefd28786a6c3c
Prashanth S <prashusvjp@gmail.com>
2021-12-16 13:35:18 +0530

    Update

--- src/backend/bank_login.php
+++ src/backend/bank_login.php
@@ -3,6 +3,7 @@ error_reporting(0);
 $data = json_decode(file_get_contents('php://input'), true);
 
 $connection = new mysqli("localhost","root","mysql");
+/*If you are using XAMPP for MySQL then edit the previous line as new mysqli("localhost","root","") */
 mysqli_select_db($connection,"db_blood_bank");
 if($connection->connect_error){
     echo "Connection failed";
